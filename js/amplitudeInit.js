3b7c68d81765535a21cf53417fcfbfa35200421c
AdamCB <adam.caplan.bricker@gmail.com>
2018-05-21 13:35:23 -0400

    audioplayer updates
    -extending amplitude.js time_update event for control over time formatting and seeking.
    
    -adding public domain images for play/pause

--- js/amplitudeInit.js
+++ js/amplitudeInit.js
@@ -1,3 +1,6 @@
+var SEEKING = false;
+var SHOWDURATION = true;
+var DURATION;
 Amplitude.init({
 
 	"songs": [
@@ -8,6 +11,135 @@ Amplitude.init({
                 "url": "https://hearclassical.github.io/HEARclassical/assets/audio/aNoteAboutWitches.mp3",
                 "cover_art_url": "https://hearclassical.github.io/HEARclassical/assets/images/poster_april28.jpg"
             }
-        ]
+        ],
+    "callbacks": {
+    	"time_update": function() {
+
+    		DURATION = Amplitude.audio().duration;
+    		if (!DURATION) {return;}
+    		if (!SEEKING) {
+    			var curPercent = Amplitude.getSongPlayedPercentage();
+    			updateProgress(curPercent / 100);
+    			var curTime = curPercent * Amplitude.audio().duration / 100;
+    			updateCurrentTime(curTime);
+    			updateEndTime(curTime);
+
+
+
+    		}
+    		
+    	}
+    }
 
 })
+
+
+
+function updateProgress(percentage) {
+	document.getElementById('song-played-progress').value = percentage;
+}
+
+function updateCurrentTime(time) {
+	var s = Math.floor(time % 60);
+	var m = Math.floor(time / 60);
+
+	var seconds = (s < 10) ? '0' + parseInt(s, 10) : '' + parseInt(s, 10);
+	var minutes = (m == 0) ? '0' : '' + m;
+	document.querySelector('.current-minutes').innerHTML = minutes;
+    document.querySelector('.current-seconds').innerHTML = seconds;
+}
+
+function updateEndTime(time) {
+	var minus;
+
+	if (SHOWDURATION) {
+		time = DURATION;
+		minus = ""
+	} else {
+		time = Math.floor(DURATION) - time;
+		minus = "-"
+	}
+
+
+	var s = Math.ceil(time % 60);
+	var m = Math.floor(time / 60);
+
+	var seconds = (s < 10) ? '0' + parseInt(s, 10) : '' + parseInt(s, 10);
+	var minutes = minus + ( (m == 0) ? '0' : '' + m );
+	document.querySelector('.duration-minutes').innerHTML = minutes;
+    document.querySelector('.duration-seconds').innerHTML = seconds;
+}
+
+
+//when duration area is clicked, switch from between duration to remaining time and 
+document.querySelector('.duration').onclick = function() {
+	if (DURATION) {
+		SHOWDURATION = !SHOWDURATION;
+		var curTime = Amplitude.getSongPlayedPercentage() * Amplitude.audio().duration / 100;
+		updateEndTime(curTime) 
+	}
+
+}
+
+
+	/*
+	key events
+
+    window.onkeydown = function(e) {
+        return !(e.keyCode == 32);
+    };
+	*/
+
+    /*
+      Handles a click on the song played progress bar.
+    */
+
+document.getElementById('song-played-progress').addEventListener('mousedown', function( e ){
+
+	SEEKING = true;
+    var offset = this.getBoundingClientRect();
+    var x = e.pageX - offset.left;
+    var newProgress = ( parseFloat( x ) / parseFloat( this.offsetWidth) ) ;
+    var newProgress = Math.min(Math.max(newProgress, 0), 1.0);
+    setTimeout(function(){document.getElementById('song-played-progress').value = newProgress;}, 5)
+
+    var newTime = newProgress * DURATION;
+
+    updateCurrentTime(newTime);
+    updateEndTime(newTime);
+    
+	document.addEventListener('mouseup', mouseUpUpdate);
+	document.addEventListener('mousemove', mouseMoveUpdate);
+});
+
+var mouseMoveUpdate = function(e) {
+	var that = document.getElementById('song-played-progress');
+	var offset = that.getBoundingClientRect();
+    var x = e.pageX - offset.left;
+    var newProgress = ( parseFloat( x ) / parseFloat( that.offsetWidth) );
+    var newProgress = Math.min(Math.max(newProgress, 0), 1.0);
+
+    var newTime = newProgress * DURATION;
+
+    updateCurrentTime(newTime);
+    updateEndTime(newTime);
+
+    document.getElementById('song-played-progress').value = newProgress;
+}
+
+var mouseUpUpdate = function(e) {
+	SEEKING = false;
+    Amplitude.setSongPlayedPercentage(100 * document.getElementById('song-played-progress').value);
+    document.removeEventListener('mouseup', mouseUpUpdate);
+    document.removeEventListener('mousemove', mouseMoveUpdate);
+
+
+}
+
+function secToMinSec(seconds) {
+	return {"minutes": Math.floor(seconds / 60), "seconds": seconds % 60}
+}
+
+function minSecToSec(minsec) {
+	return 60*minsec.minutes + minsec.seconds;
+}
