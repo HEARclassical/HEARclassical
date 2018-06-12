48a1e5e69a3fbfb74c1d099dc15f17010ef681dd
AdamCB <adam.caplan.bricker@gmail.com>
2018-06-12 16:00:06 -0400

    bad image resolutions fixed

--- assets/images/website-previews/website-preview-capture.sh
+++ assets/images/website-previews/website-preview-capture.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 overwrite="--overwrite"
 format="--format=jpg"
-##resolution="1366x768"
-resolution="342x192"
+resolution="1366x768"
+##resolution="342x192"
 while read p; do
   pageres `echo "$p $resolution $format $overwrite"`
 done <website-preview-list.txt
